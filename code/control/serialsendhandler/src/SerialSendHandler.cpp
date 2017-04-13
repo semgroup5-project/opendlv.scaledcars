2359fdb0dd8078a65cfc1185325a2ef58943dd46
Mattias Landkvist <mattias.landkvist@gmail.com>
2017-04-13 12:24:16 +0200

    Serial handler send tuning

--- code/control/serialsendhandler/src/SerialSendHandler.cpp
+++ code/control/serialsendhandler/src/SerialSendHandler.cpp
@@ -28,7 +28,15 @@ SerialSendHandler::SerialSendHandler(const int32_t &argc, char **argv) :
 SerialSendHandler::~SerialSendHandler() {}
 
 void SerialSendHandler::setUp() {
-    cout << "This method is called before the component's body is executed." << endl;
+   automotive::VehicleControl vc;
+   vc.setSteeringWheelAngle(0);
+   
+
+	// Create container for finally sending the set values for the control algorithm.
+   Container c2(vc);
+   // Send container.
+   getConference().send(c2);
+   cout << "This method is called before the component's body is executed." << endl;
 }
 
 void SerialSendHandler::tearDown() {
@@ -36,20 +44,34 @@ void SerialSendHandler::tearDown() {
 }
 
 void SerialSendHandler::nextContainer(Container &c) {
-	const string SERIAL_PORT = "/dev/ttyACM1";
+	const string SERIAL_PORT = "/dev/ttyACM0";
 	const uint32_t BAUD_RATE = 11500;
+	   cout << "Container <3" << endl;
+
+	automotive::VehicleControl vc;
+   vc.setSteeringWheelAngle(0);
+   
+
+	// Create container for finally sending the set values for the control algorithm.
+   Container c2(vc);
+   // Send container.
+   getConference().send(c2);
+
+	cout << "The correct id: " << automotive::VehicleControl::ID() << endl;
+	cout << "Type of container: " << c.getDataType() << endl;
 
     // We are using OpenDaVINCI's std::shared_ptr to automatically
     // release any acquired resources.
     
-   if (c.getDataType() == automotive::VehicleControl::ID()) {
+   //if (c.getDataType() == automotive::VehicleControl::ID()) {
+   	   cout << "if statement <3" << endl;
       const automotive::VehicleControl vd = c.getData<automotive::VehicleControl>();
-      int angle = vd.getSteeringWheelAngle();
+//      int angle = vd.getSteeringWheelAngle();
 //      int speed = vd.getSpeed();
       protocol_data pd;
       protocol_data *ppd = &pd;
       ppd->id = 2;
-      ppd->value = angle;
+      ppd->value = 0;
       
       protocol_frame pf = protocol_encode(pd);
       protocol_frame *ppf = &pf;
@@ -60,12 +82,13 @@ void SerialSendHandler::nextContainer(Container &c) {
         	std::shared_ptr<SerialPort> serial(SerialPortFactory::createSerialPort(SERIAL_PORT, BAUD_RATE));
 			
         	serial->send(test);
+        		   cout << "Sexy cream sent by serial <3" << endl;
         	//serial->send(vd.toString());
     	}
     	catch(string &exception) {
         	cerr << "Serial port could not be created: " << exception << endl;
     	}
-	}
+	//}
 }
 
 } /*---------*/
