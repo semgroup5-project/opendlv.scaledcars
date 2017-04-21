ba11c81fa9155fd52d8240bb3a9b957ecc7bcdb8
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2017-04-21 13:37:03 +0200

    refactor serial handler

--- code/control/serialsendhandler/src/SerialSendHandler.cpp
+++ code/control/serialsendhandler/src/SerialSendHandler.cpp
@@ -26,7 +26,8 @@ namespace scaledcars {
         int port = 0;
         const string SERIAL_PORTS[] = {"/dev/ttyACM0", "/dev/ttyACM1", "/dev/ttyACM2", "/dev/ttyACM3"};
         const uint32_t BAUD_RATE = 115200;
-        FILE *file;
+        int counter = 0;
+
 
         void SerialReceiveListener::nextString(const string &s) {
             cerr << "RECEIVED: " << s << " CONTAINS: " << s.length() << " BYTES!" << endl;
@@ -38,40 +39,41 @@ namespace scaledcars {
         SerialSendHandler::~SerialSendHandler() {}
 
         void SerialSendHandler::setUp() {
-            cerr << "Connecting to port: " << SERIAL_PORTS[port] << " br: " << BAUD_RATE << endl;
+            cerr << "Setting up serial handler"<< endl;
 
-            try {
-                shared_ptr <SerialPort> _serialPort(
-                        SerialPortFactory::createSerialPort(SERIAL_PORTS[port], BAUD_RATE));
-
-                this->serialPort = _serialPort;
-                this->serialPort->setStringListener(&(this->serialListener));
-                this->serialPort->start();
+//            try {
+//                shared_ptr <SerialPort> _serialPort(
+//                        SerialPortFactory::createSerialPort(SERIAL_PORTS[port], BAUD_RATE));
+//
+//                this->serialPort = _serialPort;
+//                this->serialPort->setStringListener(&(this->serialListener));
+//                this->serialPort->start();
 
                 // Wait for serial port to be ready for communication
-                cerr << "Sleeping for 5 secs" << endl;
-                const uint32_t ONE_SECOND = 1000 * 1000;
-                odcore::base::Thread::usleepFor(5 * ONE_SECOND);
+//                cerr << "Sleeping for 5 secs" << endl;
+//                const uint32_t ONE_SECOND = 1000 * 1000;
+//                odcore::base::Thread::usleepFor(5 * ONE_SECOND);
 
-            } catch (string &exception) {
-                cerr << "Serial error : " << exception << endl;
-                port++;
-                if (port < 4) {
-                    cerr << "Trying port : " << SERIAL_PORTS[port] << endl;
-                    setUp();
-                }
-            }
+//            } catch (string &exception) {
+//                cerr << "Serial error : " << exception << endl;
+//                port++;
+//                if (port < 4) {
+//                    cerr << "Trying port : " << SERIAL_PORTS[port] << endl;
+//                    setUp();
+//                }
+//            }
         }
 
         void SerialSendHandler::tearDown() {
-            cerr << "Shutting down port: " << SERIAL_PORTS[port] << endl;
-            this->serialPort->stop();
-            this->serialPort->setStringListener(NULL);
+            cerr << "Shutting down serial handler" << endl;
+//            this->serialPort->stop();
+//            this->serialPort->setStringListener(NULL);
         }
 
         void SerialSendHandler::nextContainer(Container &c) {
-            try {
+                cerr << "NEXT CONTAINER " << c.getDataType() << endl;
                 if (c.getDataType() == automotive::VehicleControl::ID()) {
+                    cerr << "VehicleControl" << endl;
                     const automotive::VehicleControl vd =
                             c.getData<automotive::VehicleControl>();
                     int arduinoAngle = 0;
@@ -79,34 +81,35 @@ namespace scaledcars {
                     cerr << "angle radius : " << angle << endl;
 
                     arduinoAngle = 90 + (angle * (180 / pi));
-
-                    // introduce top and bottom limits for angle values sent to the car
-                    if (arduinoAngle < 0 ){
-                        arduinoAngle = 10;
-                    }else if (arduinoAngle > 180){
-                        arduinoAngle = 160;
+                    if (arduinoAngle < 0) {
+                        arduinoAngle = 0;
+                    } else if(arduinoAngle > 180){
+                        arduinoAngle = 180;
                     }
                     cerr << "angle degree " << arduinoAngle << endl;
 
                     //(x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-                   // int speed = 105;
+                    // int speed = 105;
                     int speed = vd.getSpeed();
                     cerr << "speed to arduino : " << speed << endl;
-                    // TODO: int odometer = vd.getOdometer();
+//                    // TODO: int odometer = vd.getOdometer();
 
-                    string speedMessage = pack(ID_OUT_MOTOR, speed);
-                    string angleMessage = pack(ID_OUT_SERVO, arduinoAngle);
+                    string message = "m" + to_string(speed) + "t" + to_string(arduinoAngle) + "x";
 
-                    // TODO: string odometerMessage = pack(ID_OUT_ODOMETER, odometer);
+                    cerr << "speedMessage " << message << endl;
+//
+////                    string speedMessage = pack(ID_OUT_MOTOR, speed);
+////                    string angleMessage = pack(ID_OUT_SERVO, arduinoAngle);
+//
+//                    // TODO: string odometerMessage = pack(ID_OUT_ODOMETER, odometer);
+//
+                    if (counter % 30 == 0) {
+                        send(message);
 
-                    send(speedMessage);
-                    send(angleMessage);
-
-                    // TODO: send(odometerMessage);
+                    }
+                    counter++;
+//                    // TODO: send(odometerMessage);
                 }
-            } catch (string &exception) {
-                cerr << "Serial Error : " << exception << endl;
-            }
         }
 
         string SerialSendHandler::pack(int id, int value) {
@@ -127,6 +130,35 @@ namespace scaledcars {
         }
 
         void SerialSendHandler::send(string message) {
+
+            try {
+                cerr << "Connecting to port: " << SERIAL_PORTS[port] << " br: " << BAUD_RATE << endl;
+
+                shared_ptr <SerialPort> _serialPort(
+                            SerialPortFactory::createSerialPort(SERIAL_PORTS[port], BAUD_RATE));
+                this->serialPort = _serialPort;
+                this->serialPort->setStringListener(&(this->serialListener));
+                this->serialPort->start();
+
+                this->serialPort->send(message);
+
+//                cerr << "Sleeping for 1 secs" << endl;
+//                const uint32_t ONE_SECOND = 1000 * 1000;
+//                odcore::base::Thread::usleepFor(ONE_SECOND / 2);
+
+                this->serialPort->stop();
+                this->serialPort->setStringListener(NULL);
+                port = 0;
+
+                cerr << "Shutting down port: " << SERIAL_PORTS[port] << endl;
+            } catch (string &exception) {
+                cerr << exception << endl;
+                port++;
+                if (port < 4) {
+                    cerr << "Trying port : " << SERIAL_PORTS[port] << endl;
+                    send(message);
+                }
+            }
             //cerr << "sending : " << message << endl;
 
             // baudrate-adjusted throttle (determines throughput)
@@ -139,7 +171,6 @@ namespace scaledcars {
             //odcore::base::Thread::usleepFor(87);
 
 
-            this->serialPort->send(message);
             //odcore::base::Thread::usleepFor(10000);
 
 //            const uint32_t ONE_MS = 1000 * 1;
