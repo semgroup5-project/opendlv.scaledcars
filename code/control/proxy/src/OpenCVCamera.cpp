210a4abcb2f8a8fb7aca127fee0e65a366f97572
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2017-04-28 09:30:52 +0200

    deleted seriareceive folder modified armhf compose to try on rpi3

--- code/control/proxy/src/OpenCVCamera.cpp
+++ code/control/proxy/src/OpenCVCamera.cpp
@@ -23,24 +23,27 @@
 #include "opencv2/imgproc/imgproc.hpp"
 
 #include "OpenCVCamera.h"
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
 
 namespace scaledcars {
-namespace control {
+    namespace control {
 
-        OpenCVCamera::OpenCVCamera(const string &name, const uint32_t &id, const uint32_t &width, const uint32_t &height, const uint32_t &bpp) :
-            Camera(name, id, width, height, bpp),
-            m_capture(NULL),
-            m_image(NULL),
-            m_sharedImageMemory(){
+        OpenCVCamera::OpenCVCamera(const string &name, const uint32_t &id, const uint32_t &width,
+                                   const uint32_t &height, const uint32_t &bpp) :
 
+                Camera(name, id, width, height, bpp),
+                m_capture(NULL),
+                m_image(NULL),
+                m_sharedImageMemory() {
             m_capture = cvCaptureFromCAM(id);
             if (m_capture) {
                 cvSetCaptureProperty(m_capture, CV_CAP_PROP_FRAME_WIDTH, width);
                 cvSetCaptureProperty(m_capture, CV_CAP_PROP_FRAME_HEIGHT, height);
                 cvSetCaptureProperty(m_capture, CV_CAP_PROP_FPS, 30);
-      //          cvSetCaptureProperty(m_capture, CV_CAP_PROP_FOCUS_MOD, 0);
-            }
-            else {
+                //          cvSetCaptureProperty(m_capture, CV_CAP_PROP_FOCUS_MOD, 0);
+            } else {
                 cerr << "proxy: Could not open camera '" << name << "' with ID: " << id << endl;
             }
         }
@@ -68,8 +71,7 @@ namespace control {
                         }
 
                         cvCvtColor(tmpFrame, m_image, CV_BGR2GRAY);
-                    }
-                    else {
+                    } else {
                         m_image = cvRetrieveFrame(m_capture);
                     }
 
@@ -82,7 +84,7 @@ namespace control {
         bool OpenCVCamera::copyImageTo(char *dest, const uint32_t &size) {
             bool retVal = false;
 
-            if ( (dest != NULL) && (size > 0) && (m_image != NULL) ) {
+            if ((dest != NULL) && (size > 0) && (m_image != NULL)) {
                 memcpy(dest, m_image->imageData, size);
 
                 cvShowImage("WindowShowImage", m_image);
