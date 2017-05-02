24325327e4ceac1225a239bacb07c3b3971a5a7f
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2017-05-02 19:35:51 +0200

    armhf script

--- scripts/runarmhf.sh
+++ scripts/runarmhf.sh
@@ -4,5 +4,5 @@ echo "Removing file /dev/video0..." >&2
 sudo rm -f /dev/video0
 sudo ln -f /dev/video8 /dev/video0
 echo "Dev symlink created! Please set camId to 0" >&2
-cd $HOME/opendlv.scaledcars/usecases/armhf/DIT-168 && ./setupODVconfig.sh && cd $HOME/opendlv.scaledcars/usecases/armhf && xhost + && docker-compose down && docker-compose up
+cd $HOME/opendlv.scaledcars/usecases/armhf/DIT-168 && ./setupODVconfig.sh && cd $HOME/opendlv.scaledcars/usecases/armhf && docker-compose down && docker-compose up
 
