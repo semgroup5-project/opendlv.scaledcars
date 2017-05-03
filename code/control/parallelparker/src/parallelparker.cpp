529bc437b4cb70cb2726251b5d0070c1e66e43d6
geoffrey <chyj1993@gmail.com>
2017-05-03 09:54:29 +0200

    one more parking situation covered

--- code/control/parallelparker/src/parallelparker.cpp
+++ code/control/parallelparker/src/parallelparker.cpp
@@ -48,7 +48,7 @@ namespace scaledcars {
 
         void parallelparker::setUp() {
             // This method will be call automatically _before_ running body().
-           // sim = getKeyValueConfiguration().getValue<int32_t>("parallelparking.simu");
+            // sim = getKeyValueConfiguration().getValue<int32_t>("parallelparking.simu");
             KeyValueConfiguration kv = getKeyValueConfiguration();
             sim = (kv.getValue<int32_t>("parallelparker.sim") == 0);
         }
@@ -86,11 +86,13 @@ namespace scaledcars {
             double parkEnd = 0;
             double parkingGap = 0;
             double counter = 1;
+            double i = 0;
 
             //double irFrontRight = 0;
             double usFront = 0;
             double irRear = 0;
             double irRearRight = 0;
+            int parkingSit = 0; //0 -> Two boxes, 1 -> Enough space from beginning,
             int gap = 0;
             int stageMeasuring = 0;
             int stageMoving = 0;
@@ -124,15 +126,19 @@ namespace scaledcars {
                     if (stageMoving == 1) {
                         vc.setSpeed(0);
                         vc.setSteeringWheelAngle(0);
-                        if (irRearRight > 0) {
+                        if (irRearRight > 0 && parkingSit == 1) {
                             stageMoving = 2;
+                        }
+                        if (irRearRight < 0 && parkingSit == 2) {
+                            stageMoving = 3;
+                            cerr << "parkingSit ==" << parkingSit << " moving to next stage" << endl;
                         }
                     }
                     if (stageMoving == 2) {
-                        vc.setSpeed(-1);
-                        stageMoving = 2;
+                        vc.setSpeed(1);
                         if (irRear < 0) {
                             stageMoving = 3;
+                            cerr<<"irRear smaller than 0 Stage Moving = 3"<<endl;
                         }
                     }
 
@@ -152,11 +158,9 @@ namespace scaledcars {
                                 counter++;
                             }
                             if (irRear < 0 && adjDist > parkingGap / 2 && counter > 1) {
-
                                 vc.setSpeed(-1);
                                 vc.setSteeringWheelAngle(-2);
-
-                                counter -= 1.5;
+                                counter -= 1.6;
                             }
                         }
                         if (gap == 2) {
@@ -173,32 +177,43 @@ namespace scaledcars {
 
                                 vc.setSteeringWheelAngle(-.3);
 
-                                counter -= 1.5;
+                                counter -= 1.6;
                             }
                         }
+                        cerr<< "counter "<< counter <<endl;
                     }
+
                     if (counter < 1) {
+                        i = vd.getAbsTraveledPath();
                         stageMoving = 4;
                     }
                     if (stageMoving == 4) {
-                        cerr<< "Move one last bit"<<endl;
+                        cerr << "Move one last bit" << endl;
+                        double j = vd.getAbsTraveledPath();
                         vc.setSpeed(1);
                         vc.setSteeringWheelAngle(.2);
-
-                        if (usFront < 7 && usFront > 0) {
+                        if(parkingSit ==2 && j-i < 2){
+                            cerr<<"stage4 #1 if"<<endl;
                             stageMoving = 5;
                         }
+                        if (usFront < 7 && usFront > 0) {
+                            stageMoving = 5;
+                            cerr<<"stage4 #2 if"<<endl;
+                        }
+                        
                     }
                     if (stageMoving == 5) {
+                        cerr<<"car stopped!!"<<endl;
                         vc.setSpeed(0);
                         vc.setSteeringWheelAngle(0);
                     }
                     if (irRear > 0 && irRear < 4) {
                         //Emergency stop
                         vc.setSpeed(0);
+                        cerr << "emergency stop!"<<endl;
                     }
-                }else if(!sim){
-                    cerr<<"This is for the car!"<<endl;
+                } else if (!sim) {
+                    cerr << "This is for the real car!" << endl;
                 }
 
 
@@ -206,24 +221,35 @@ namespace scaledcars {
 
                     switch (stageMeasuring) {
                         case 0: {
-
-                            // Initialize measurement.
-
                             distance = sbd.getValueForKey_MapOfDistances(INFRARED_FRONT_RIGHT);
-                            if ((IFFRObstacle)) {
+                            double parking = vd.getAbsTraveledPath();
+                            absPathStart = vd.getAbsTraveledPath();
+                            //when there is no car parked for 7 units
+                            if (!IFFRObstacle && parking > 15) {
+                                GAP_SIZE = parking;
+                                parkingGap = parking;
+                                gap = 1;
+                                stageMoving = 1;
+                                parkingSit = 2;
+                                stageMeasuring ++;
+                            }
+                            //when there is something detected within 7 units
+                            if (parking < 7 && IFFRObstacle) {
                                 stageMeasuring++;
+                                parkingSit = 1;
                             }
                         }
                             break;
                         case 1: {
-
                             // Checking for sequence +, -.
-                            if ((distance > 0) && (sbd.getValueForKey_MapOfDistances(INFRARED_FRONT_RIGHT) < 0)) {
-                                // Found sequence +, -.
-                                stageMeasuring = 2;
-                                absPathStart = vd.getAbsTraveledPath();
+                            if (parkingSit == 1) {
+                                if ((distance > 0) && (sbd.getValueForKey_MapOfDistances(INFRARED_FRONT_RIGHT) < 0)) {
+                                    // Found sequence +, -.
+                                    stageMeasuring = 2;
+                                    absPathStart = vd.getAbsTraveledPath();
+                                }
+                                distance = sbd.getValueForKey_MapOfDistances(INFRARED_FRONT_RIGHT);
                             }
-                            distance = sbd.getValueForKey_MapOfDistances(INFRARED_FRONT_RIGHT);
                         }
                             break;
                         case 2: {
@@ -232,18 +258,17 @@ namespace scaledcars {
                                 absPathEnd = vd.getAbsTraveledPath();
                                 GAP_SIZE = (absPathEnd - absPathStart);
                                 cerr << "Size = " << GAP_SIZE << endl;
-                                if (GAP_SIZE < 15) {
+                                if (GAP_SIZE <= 10) {
                                     gap = 1;
-                                } else if (GAP_SIZE > 15) {
+                                } else if (GAP_SIZE > 10) {
                                     gap = 2;
                                 }
-                                cerr << "gap: " << gap << endl;
                                 stageMeasuring = 1;
                                 if ((stageMoving < 1) &&
                                     (sbd.getValueForKey_MapOfDistances(INFRARED_FRONT_RIGHT) > 0) && (GAP_SIZE > 7)) {
                                     stageMeasuring = 1;
                                     parkingGap = GAP_SIZE;
-                                    //parkingGap = 15;
+                                    parkingSit = 1;
                                     stageMoving = 1;
                                     cerr << "Size = " << GAP_SIZE << endl;
                                 }
